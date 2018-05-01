0f4f8902f71fc051d2493ee117ecba91b52b525a
ㅤㅤ <ccintj@gmail.com>
2018-05-01 12:25:40 -0400

    Update press.js
    Decided I prefer querySelector over getElementsBy. Also decided to create specific functions for specific cases so I don't have to hunt for node names more than once. There are easier ways to do this but I am a fool.

--- press.js
+++ press.js
@@ -1,9 +1,14 @@
-var elements = document.getElementsByClassName("classNameHere");
-//nodelist of elements, can exchange with id or tag or whatever
-function press(){
+//unfavorite twitter likes
+//Scroll as far bottom as possible on the web app
+//Run this 
+var elements = document.querySelectorAll(".ProfileTweet-action--unfavorite");
+function unfavorite(){
 	for(var i = 0; i<elements.length;i++){
 	elements[i].click();
 } //function to press all the buttons
 }
-press(); //crank it to 11
-//sidenote: if you run this while the follow button is pressed you WILL unfollow those people
+unfavorite();
+
+
+
+
