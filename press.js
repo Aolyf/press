85925eb6e0f0485bf96a7d5fb49abe3311604720
Aolyf <ccintj@gmail.com>
2018-05-29 22:54:59 -0400

    Update press.js

--- press.js
+++ press.js
@@ -2,12 +2,22 @@
 //Scroll as far bottom as possible on the web app
 //Run this 
 var elements = document.querySelectorAll(".ProfileTweet-action--unfavorite");
-function unfavorite(){
+function unfavoriteTwitter(){
 	for(var i = 0; i<elements.length;i++){
 	elements[i].click();
 } //function to press all the buttons
 }
-unfavorite();
+unfavoriteTwitter();
+
+var elements = document.querySelectorAll("._qfo _4jy0 _4jy3 _517h _51sy _42ft");
+function inviteFB(){	
+	for(var i = 0; i<elements.length;i++){
+	elements[i].click();
+	}
+}
+inviteFB();
+		
+	
 
 
 
